ad94f5ac7f52fa4c8028004b7503eedcd65f8069
Dutch Nguyen <thuannguyen1607.buy@gmail.com>
2026-07-02 20:45:52 +0700

    Navigate DB config to .env file

--- src/db_engine.py
+++ src/db_engine.py
@@ -1,14 +1,24 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
+from dotenv import load_dotenv, find_dotenv  
+
+import time
+from sqlalchemy.exc import OperationalError
+
+
+load_dotenv(find_dotenv())
 
 # ================== CONFIG ==================
 def get_engine():
     DB_USER = os.environ.get("MYSQL_ROOT_USER", "root")
-    DB_PASS = os.environ.get("MYSQL_ROOT_PASSWORD", "root123")
-    DB_HOST = "mysql"  
-    DB_PORT = "3306"
+    DB_PASS = os.environ.get("MYSQL_ROOT_PASSWORD") 
+    DB_HOST = os.environ.get("MYSQL_HOST", "mysql")  
+    DB_PORT = os.environ.get("MYSQL_PORT", "3306")
     DB_NAME = os.environ.get("MYSQL_DATABASE", "logistics_otd")
+
+    if not DB_PASS:
+        raise ValueError("🚨 Error: 'MYSQL_ROOT_PASSWORD' is missing in your .env file!")
 
     connection_url = URL.create(
         drivername="mysql+pymysql",
@@ -29,3 +39,20 @@ def get_engine():
     
     return engine
 
+#Engine Connection Testing
+if __name__ == "__main__":
+    engine = get_engine()
+    print("📤 Checking connection to MySQL...")
+    retries = 6
+    while retries > 0:
+        try:
+            with engine.connect() as connection:
+                print("⚙️ Internal network connection successful! MySQL is ready.")
+                break
+        except OperationalError:
+            retries -= 1
+            print(f"⏳ MySQL is busy or starting up, waiting 5 seconds... ({retries} retries remaining)")
+            time.sleep(5)
+            
+    if retries == 0:
+        print("❌ Failure: Unable to connect to the database.")
